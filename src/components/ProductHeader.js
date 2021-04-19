f0078ab6e29297366a866bb988ff6b05e564852c
hokomili <28939939+hokomili@users.noreply.github.com>
2021-04-19 22:40:13 +0800

    category page img and title

--- src/components/ProductHeader.js
+++ src/components/ProductHeader.js
@@ -1,9 +1,13 @@
 export default function ProductHeader({title}) {
+   const url = window.location.pathname;
     return (
        <div className="header">
-          <div className="ph-bg"></div>
+            <div className={url.slice(0,4)==='/Map'?
+                     "ph-bg":url.slice(0,4)==='/Tex'?"ph2-bg":"ph3-bg"}>
+                   </div>
           <div className="ph-box">
-             <div className="ph-text">MAPS</div>
+             <div className="ph-text">{url.slice(0,4)==='/Map'?
+                     "Maps":url.slice(0,4)==='/Tex'?"Texture Packs":"Mods"}</div>
           </div>
  
        </div>
